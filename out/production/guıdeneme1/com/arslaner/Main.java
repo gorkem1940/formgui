08aa6afe511d33298d2b2259b31db364f465f36b
gorkem <113816295+gorkem1940@users.noreply.github.com>
2022-09-26 22:59:50 +0300

    Create Main.java

---
+++ out/production/guıdeneme1/com/arslaner/Main.java
@@ -0,0 +1,14 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.arslaner;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+    }
+}
